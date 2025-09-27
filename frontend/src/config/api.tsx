168eb4da5edd6096997989c768d9664ff57c2cb2
Rasiga-Dev <rasiga04112002@gmail.com>
2025-09-27 16:53:23 +0530

    Updated API URL and config

--- frontend/src/config/api.tsx
+++ frontend/src/config/api.tsx
@@ -1,3 +1,5 @@
 // src/config/api.tsx
-const VITE_BASE_URL = "http://localhost:11129/api";
+// const VITE_BASE_URL = "http://localhost:11129/api";
+
+const VITE_BASE_URL = import.meta.env.VITE_API_URL;
 export default VITE_BASE_URL;
